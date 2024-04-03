8a91d0541f902ffd8dc8a82334322bdaee852561
alexandrea parks <alparks@idk.com>
2024-04-03 18:19:10 -0500

    added initial bmi-calculator code

---
+++ lab-11-bmi-pinia/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+// PARENT
+
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+
+import { ref } from 'vue'
+
+const bmiResult = ref('')
+
+// this function is called when the Child component emits an event
+function calculateBMI(weight, height) {  // arguments come from 2 values in Child event emitted
+  const bmi = (weight / ( height * height )).toFixed(2)
+  bmiResult.value = `Your BMI is: ${bmi}`
+}
+
+</script>
+
+<template>
+
+  <div id="app-component">
+
+    <h1>Body Mass Index Calculator</h1>
+
+    <!--v-on receives event emitted from Child and calls calculateBMI function-->
+    <BodyMassIndexForm v-on:stats-entered="calculateBMI"></BodyMassIndexForm>
+
+    <p id="bmi-result-paragraph">{{ bmiResult }}</p>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+#app-component {
+  background-color: yellowgreen;
+  padding: 40px;
+}
+
+#bmi-result-paragraph {
+  font-size: 50px;
+}
+
+</style>
---
+++ lab-11-bmi-pinia/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,61 @@
+<script setup>
+// CHILD
+
+import { ref } from 'vue'
+
+const emit = defineEmits([
+  'stats-entered'
+])
+
+// reactive data to store height and weight user enters
+const weight = ref('')
+const height = ref('')
+
+function calculateClicked () {
+  // validate that both height and weight were entered
+  if (weight.value === '' || height.value === '') {
+    alert('Enter a height and weight!')
+  } else {
+    // emit event to tell the Parent component that the user clicked on Calculate button
+    // include weight and height values
+    emit('stats-entered', weight.value, height.value)
+  }
+
+}
+
+</script>
+
+<template>
+
+  <div id="bmi-form">
+
+    <h2>Enter your height and weight</h2>
+
+    <!--v-model links the input values with the reactive data weight and height-->
+    <label for="height-meters">Height in meters</label>
+    <input v-model="height" type="number" id="height-meters">
+    <br>
+    <label for="weight-kilograms">Weight in kilograms</label>
+    <input v-model="weight" type="number" id="weight-kilograms">
+    <br>
+    <!--v-on click calls calculateClicked function that will emit the event and values to Parent component-->
+    <button v-on:click="calculateClicked" id="calculate-button">Calculate</button>
+
+
+  </div>
+
+</template>
+
+<style scoped>
+
+#bmi-form {
+  background-color: darkblue;
+}
+
+#calculate-button {
+  background-color: indianred;
+  margin: 30px;
+}
+
+</style>
+
